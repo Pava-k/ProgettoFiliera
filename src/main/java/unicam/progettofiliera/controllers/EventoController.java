a0a8d4da31eedd470561917c90eec2604d10aa30
Tom77003 <fioravantitommaso77@gmail.com>
2025-07-10 17:12:33 +0200

    Accorgimenti vari

--- src/main/java/unicam/progettofiliera/controllers/EventoController.java
+++ src/main/java/unicam/progettofiliera/controllers/EventoController.java
@@ -1,11 +1,9 @@
 package unicam.progettofiliera.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import unicam.progettofiliera.infrastructure.AnimatoreRepository;
 import unicam.progettofiliera.infrastructure.EventoRepository;
 import unicam.progettofiliera.models.Animatore;
@@ -13,18 +11,18 @@ import unicam.progettofiliera.models.Evento;
 import unicam.progettofiliera.service.EventoHandler;
 
 @RestController
+@RequestMapping("/BachecaEventi")
 public class EventoController {
     private final EventoHandler eventoHandler;
-    private final AnimatoreRepository animatoreRepository;
 
-    public EventoController(EventoRepository eventoRepository, AnimatoreRepository animatoreRepository, EventoHandler eventoHandler) {
+    @Autowired
+    public EventoController(EventoHandler eventoHandler) {
         this.eventoHandler = eventoHandler;
-        this.animatoreRepository = animatoreRepository;
     }
     @PostMapping("/add/{id}")
-    public ResponseEntity<Object> creaEvento(@PathVariable Long id, @RequestBody Evento evento){
+    public ResponseEntity<Evento> pubblicaEvento(@PathVariable Long id, @RequestBody Evento evento){
         eventoHandler.caricaEvento(id,evento);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(evento);
 
     }
 }
--- src/main/java/unicam/progettofiliera/models/Animatore.java
+++ src/main/java/unicam/progettofiliera/models/Animatore.java
@@ -12,22 +12,17 @@ public class Animatore extends UtenteRegistrato{
 
     @OneToMany(mappedBy = "animatore", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Evento> eventiPubblicati = new ArrayList<>();
-
     //TODO private CasellaPostale casellaPostale
 
     public Animatore() {
-
     }
 
 
     public Animatore(String nome, String password) {
-
         super(nome, password);
-
     }
 
     public List<Evento> getEventiPubblicati() {
-
         return eventiPubblicati;
     }
 }
